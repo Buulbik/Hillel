40a0500e5b239b6658eff6a6eff4ef118a22569a
Buulbik <forclasssrooom@gmail.com>
2022-08-01 18:12:30 +0300

    Изображения блога

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from config.views import home
+from django.conf.urls.static import static
+from config import settings
 
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include("apps.blog.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
