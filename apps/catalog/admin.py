6f7c06f7bb16d971cfaeb918962c2b4a9cb21f47
Buulbik <forclasssrooom@gmail.com>
2022-08-18 18:12:57 +0300

    Product model

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.catalog.models import Category, Product
+from apps.catalog.models import Category, Product, Image
 
 
 @admin.register(Category)
@@ -12,12 +12,19 @@ class ProductCategoryInline(admin.TabularInline):
     extra = 1
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ['product', 'image_tag', 'image', 'is_main']
+    readonly_fields = ['image_tag']
+    extra = 1
+
+
 @admin.register(Product)
 class Product(admin.ModelAdmin):
     list_display = ['id', 'name', 'quantity', 'price']
     list_display_links = ['id', 'name']
     fields = ['name', 'description',  'quantity', 'price']
-    inlines = [ProductCategoryInline]
+    inlines = [ProductCategoryInline, ImageInline]
 
 
 
