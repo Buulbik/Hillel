e21ada7b5e08574d2a4ce27549b1b875c2905d22
Buulbik <forclasssrooom@gmail.com>
2022-09-01 18:05:51 +0300

    register

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,4 +4,5 @@ from apps.user import views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
